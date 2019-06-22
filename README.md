cefb4285481f012aeef99abd5682a5c2e419301b
Oleg Natsevsky <natsevsky@gmail.com>
2019-06-22 20:17:04 +0300

    datastore emulator

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # gae_bakend_bootstrap
+
+
+Run containers
+
+docker-compose build
+
+docker-compose up -d
+
+The -d flag is used to run containers in the background.
---
+++ services/users/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Waiting for datastore emulator..."
+
+while ! nc -z datastore 8081; do
+  sleep 0.1
+done
+
+echo "Datastore emulator started"
+
+python manage.py run -h 0.0.0.0
