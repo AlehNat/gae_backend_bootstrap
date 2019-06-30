97de20cb2e86f45f04e118ae6c291acff6d76211
Oleg Natsevsky <natsevsky@gmail.com>
2019-06-30 22:17:04 +0300

    Add input validation

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,8 +1,15 @@
 from flask import request
-from flask_restplus import Resource, Namespace
+from flask_restplus import Resource, Namespace, fields
 
 
 ns_user = Namespace("users", description="Users related operations")
+user_model = ns_user.model(
+    "user",
+    {
+        "email": fields.String(required=True, description="user email address"),
+        "username": fields.String(required=True, description="user username"),
+    },
+)
 
 
 @ns_user.route("/ping")
@@ -13,6 +20,8 @@ class UsersPing(Resource):
 
 @ns_user.route("/")
 class UsersList(Resource):
+    @ns_user.doc("create a new user")
+    @ns_user.expect(user_model, validate=True)
     def post(self):
         from project.api.service.storage.users import store_user
 
